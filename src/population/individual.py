c31483a4c47a191525236fe6680a9524fc0c2a30
arthurlba1 <1arthurluiz1@gmail.com>
2022-04-27 17:33:03 -0300

    individual class to with data to add on population

---
+++ src/population/individual.py
@@ -0,0 +1,22 @@
+"""Handles individual of algorithm"""
+
+
+class Individual:
+
+    def __init__(
+        self,
+        x1: int,
+        x2: int,
+        x3: int,
+        x4: int,
+        x5: int,
+    ):
+        self.x1 = x1
+        self.x2 = x2
+        self.x3 = x3
+        self.x4 = x4
+        self.x5 = x5
+        self.weight = (self.x5**5)+(self.x4**4)+(self.x3**3)+(self.x2**2)+(self.x1**1)
+
+    def __str__(self):
+        return f'Individual = {self.weight}'
