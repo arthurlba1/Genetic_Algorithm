234b413a51de68da0cdd7bbf15f98847b0fca2d7
arthurlba1 <1arthurluiz1@gmail.com>
2022-04-27 18:32:49 -0300

    elitism method to pass on to new generation

--- src/population/population.py
+++ src/population/population.py
@@ -1,5 +1,8 @@
 """Handles individual of algorithm"""
 import random
+from typing import List
+
+from pyrfc3339 import generate
 
 from individual import Individual
 
@@ -7,7 +10,7 @@ from individual import Individual
 class Population(Individual):
 
 
-    def generate_population(size: int):
+    def generate_population(size: int) -> List[str]:
         cont = 0
         population = []
         while cont < size:
@@ -18,7 +21,20 @@ class Population(Individual):
             x5 = random.random()
             population.append(Individual(x1, x2, x3, x4, x5).__str__())
             cont = cont + 1
+            population.sort(reverse = True)
         return population
 
+    def three_best_individuals(array: List[str]) -> List[str]:
+        three_best = []
+        
+        for i in range(3):   
+            three_best.append(array[i])
+        
+        return three_best
 
+    def new_generation():
+        population = []
 
+population = Population.generate_population(10)
+population_elite = Population.three_best_individuals(population)
+print(population_elite)
