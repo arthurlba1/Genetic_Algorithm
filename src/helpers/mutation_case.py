044ebbc152f2f015b5c55d4c0880f97079eb632f
arthurlba1 <1arthurluiz1@gmail.com>
2022-04-28 14:33:20 -0300

    update fitness of individual

--- src/helpers/mutation_case.py
+++ src/helpers/mutation_case.py
@@ -10,16 +10,21 @@ class MutationCase:
     def mutation_case_elseif(comparator: int, individual: Individual):
         if comparator == 1:
             individual.x1 = random.random()
+            individual.fitness = (individual.x5**5)+(individual.x4**4)+(individual.x3**3)+(individual.x2**2)+(individual.x1**1)
             return individual
         elif comparator == 2:
             individual.x2 = random.random()
+            individual.fitness = (individual.x5**5)+(individual.x4**4)+(individual.x3**3)+(individual.x2**2)+(individual.x1**1)
             return individual
         elif comparator == 3:
             individual.x3 = random.random()
+            individual.fitness = (individual.x5**5)+(individual.x4**4)+(individual.x3**3)+(individual.x2**2)+(individual.x1**1)
             return individual
         elif comparator == 4:
             individual.x4 = random.random()
+            individual.fitness = (individual.x5**5)+(individual.x4**4)+(individual.x3**3)+(individual.x2**2)+(individual.x1**1)
             return individual
         elif comparator == 5:  
             individual.x5 = random.random()
+            individual.fitness = (individual.x5**5)+(individual.x4**4)+(individual.x3**3)+(individual.x2**2)+(individual.x1**1)
             return individual
