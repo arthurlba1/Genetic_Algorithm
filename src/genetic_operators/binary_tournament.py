fa69fdccfbc5799d84bef19bf386e26fe151c65c
arthurlba1 <1arthurluiz1@gmail.com>
2022-04-27 21:10:36 -0300

    mutation method to generate new individuals

--- src/genetic_operators/binary_tournament.py
+++ src/genetic_operators/binary_tournament.py
@@ -2,6 +2,7 @@
 from typing import List
 import random
 
+from src.helpers.mutation_case import MutationCase
 from src.population.individual import Individual
 from src.population.population import Population
 
@@ -10,7 +11,7 @@ class BinaryTournament:
     
     def select_tournament(population: Population, mutation_rate: int) -> List[str]:
         crossover_individuals = []
-        for i in range(int(len(population)*(100-mutation_rate)/100)):
+        for i in range(int(len(population-3)*(100-mutation_rate)/100)):
             individual_one = random.choice(population)
             individual_two = random.choice(population)
             x1 = individual_one.x1
@@ -19,3 +20,15 @@ class BinaryTournament:
             x4 = individual_two.x4
             x5 = individual_two.x5
             crossover_individuals.append(Individual(x1, x2, x3, x4, x5))
+        return crossover_individuals
+    
+    def mutation(population: Population, mutation_rate):
+        mutation_list = []
+        for i in range(int(len(population-3)*mutation_rate/100)):
+            individual = random.choice(population)
+            mutation_one = random.randint(1,5)
+            print(f'{individual} before')
+            individual = MutationCase.mutation_case_elseif(mutation_one, individual)
+            print(f'{individual} after')
+            mutation_list.append(individual)
+        return mutation_list
